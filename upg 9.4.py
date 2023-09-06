4db486e97ade9f977d70a090552ed3fd41fe639d
LinaRomilson <lina@romilson.se>
2023-09-06 18:21:48 +0200

    Uppgift gjord

---
+++ upg 9.4.py
@@ -0,0 +1,6 @@
+todos = ['Städa', 'Handla', 'Plugga', 'Ge blod']
+ui_width = 14
+print('.:TODOIFY:.'.center(ui_width))
+print('*' * ui_width)
+for element in range(len(todos)):
+    print('-', todos[element])
