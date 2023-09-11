658130f47ecadfb880026676811a6a327ddb6860
LinaRomilson <lina@romilson.se>
2023-09-11 13:35:15 +0200

    Uppgift gjord

---
+++ upg 12.2.py
@@ -0,0 +1,12 @@
+notes = {
+    "Kom ihåg!": "Ta bilen till verkstaden",
+    "Meddelande från skolan": "Friluftsdag på tisdag",
+    "Inför tentamen": "Gör alla instuderingsuppgifter"
+}
+
+print(".: ANTECKNINGAR :.".center(21))
+print("*" * 21)
+
+for note in notes:
+   print(note)
+print("-" * 21)
