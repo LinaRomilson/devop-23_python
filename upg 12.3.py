87cec50dff7db2c1ab95901458136861bd777411
LinaRomilson <lina@romilson.se>
2023-09-11 13:48:43 +0200

    Uppgift gjord

---
+++ upg 12.3.py
@@ -0,0 +1,10 @@
+notes = {
+    "Kom ihåg!": "Ta bilen till verkstaden",
+    "Meddelande från skolan": "Friluftsdag på tisdag",
+    "Inför tentamen": "Gör alla instuderingsuppgifter"
+}
+ui_width = 5
+for note, meddelande in notes.items():
+    print("-----")
+    print(f"Titel: {note}\nText: {meddelande}")
+
