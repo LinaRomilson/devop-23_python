7e63fce4e009e181745cab1bb76b0819eef7df8a
LinaRomilson <lina@romilson.se>
2023-09-11 13:50:03 +0200

    Tog bort onödig variabel

--- upg 12.3.py
+++ upg 12.3.py
@@ -3,7 +3,7 @@ notes = {
     "Meddelande från skolan": "Friluftsdag på tisdag",
     "Inför tentamen": "Gör alla instuderingsuppgifter"
 }
-ui_width = 5
+
 for note, meddelande in notes.items():
     print("-----")
     print(f"Titel: {note}\nText: {meddelande}")
