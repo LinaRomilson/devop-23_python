a2171c36d5452e33a1378b58077f83de631b206a
LinaRomilson <lina@romilson.se>
2023-09-12 14:54:48 +0200

    En bit på vägen

--- upg 11.3.py
+++ upg 11.3.py
@@ -1,5 +1,52 @@
 import json
+import os
 ui_width = 21
+
+def Hämtaheltal():
+    TidigareHeltal = {}
+    if os.path.exists('TidigareHeltal.json'):
+        with open('TidigareHeltal.json', 'r') as json_fil:
+            try:
+                TidigareHeltal = json.load(json_fil)
+            except json.decoder.JSONDecodeError:
+                TidigareHeltal = {}
+    return TidigareHeltal
+
+def SparaHeltal(TidigareHeltal):
+    with open('TidigareHeltal.json', 'w') as json_fil:
+        json.dump(TidigareHeltal, json_fil)
+
+def huvudprogram():
+    TidigareHeltal = Hämtaheltal()
+
+    while True:
+        try:
+            inmatning = input('Ange ett heltal(eller "0" för att avsluta): ')
+
+            if inmatning.lower() == '0':
+                break
+
+            heltal = int(inmatning)
+
+            if heltal not in TidigareHeltal:
+                TidigareHeltal[heltal] = None
+                print(f"{heltal} har lagts till i dictionaryn")
+            else:
+                print(f"{heltal} finns redan i dictionaryn")
+
+        except ValueError:
+            print("Ogiltig inmatning. Försök igen.")
+
+    print('Tidigare inmatade heltal.')
+    for heltal in TidigareHeltal:
+        print(heltal)
+
+    SparaHeltal(TidigareHeltal)
+
+    SparaHeltal(TidigareHeltal)
+
+if __name__ == "__main__":
+    huvudprogram()
 
 print('.: intMEMORIZER :.'.center(ui_width))
 print('*' * ui_width)
